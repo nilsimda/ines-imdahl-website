678e3c905ab4997d790813069a92d5eb99054390
Nils Imdahl <imdahlnils@gmail.com>
2025-01-06 22:19:27 +0100

    make background an actual image, not div with style

--- src/components/ui/BackgroundImage.jsx
+++ src/components/ui/BackgroundImage.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 function BackgroundImage({ imageUrl, imageUrlSmall }) {
     return (
         <>
-            <div
-                className="absolute brightness-90 inset-0 bg-cover bg-center z-[-3]"
-                style={{
-                    backgroundImage: `image-set(
-                    url(${imageUrlSmall}) 1x,
-                    url(${imageUrl}) 2x 
-                )`,
-                }}
+            <img
+                src={imageUrlSmall}
+                className="absolute brightness-90 inset-0 object-cover h-full w-full z-[-3]"
+                srcSet={`${imageUrlSmall} 1x, ${imageUrl} 2x`}
+                alt="Background Image of Ines Imdahl"
+                loading="eager"
+                fetchpriority="high"
+                decoding="async"
             />
             <div
                 className="fixed inset-0 z-[-2] bg-gradient-to-r from-pink-500/10 to-pink-300/10 mix-blend-overlay"
