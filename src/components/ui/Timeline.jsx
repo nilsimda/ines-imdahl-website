b5385cfbe78dd3ae19f14d29091febb43b072b20
Nils Imdahl <imdahlnils@gmail.com>
2025-01-07 22:45:37 +0100

    update about

--- src/components/ui/Timeline.jsx
+++ src/components/ui/Timeline.jsx
@@ -4,12 +4,15 @@ export default function Timeline() {
             {/* Timeline items with enhanced visual elements */}
             {[
                 "Studium der Psychologie an der Universität zu Köln",
-                "Hochzeit mit meiner großen Liebe Jens Lönneker",
-                "2000, 2004, 2007, 2009 Geburten unserer vier fantastischen Kinder",
-                "2011 Gründung des rheingold salon als psychologische Forschungsagentur",
-                "2016 & 2022 Erscheinen meiner Bücher",
-                "2022 gewählt zu den 40 over 40 Germanys most inspiring women",
-                "2024 gewählt zur LinkedIn TopVoice (und immer häufiger nennt man mich LinkedIn Influencerin)"
+                "Hochzeit mit meiner großen Liebe Jens Lönneker: 1999",
+                "Geburten unserer vier fantastischen Kinder: 2000, 2004, 2007 & 2009",
+                "Gründung des rheingold salon als psychologische Forschungsagentur",
+                "Handelsblattkolumnistin für den Werberat",
+                "Mein erstes Buch „Werbung auf der Couch“, 2016",
+                "Mein zweites Buch „Warum Frauen die Welt retten werden“, 2022",
+                "40 over 40 Germanys most inspiring women: 2022",
+                "LinkedIn TopVoice & wohl sowas wie LinkedIn Influenceri, 2024",
+                "LinkedIn Learning Trainerin zum Thema Marktforschung DACH, 2024",
             ].map((item, index) => (
                 <div key={index} className="relative group">
                     <div className="absolute -left-10 top-1 w-4 h-4 bg-gray-300 rounded-full group-hover:bg-gray-400 transition-colors duration-300"></div>
